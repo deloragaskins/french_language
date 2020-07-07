af05025f085571106ffb13eab01a15ed0c2016ea
DeloraGaskins <delora.gaskins@gmail.com>
2020-07-07 04:05:44 +0200

    created user profile

---
+++ header.py
@@ -0,0 +1,14 @@
+user='delora_Win10'
+
+import numpy as np
+import sys
+######################################
+#input management
+local_data_dir1='C:\\Users\\delor\\OneDrive\\french_language_output'
+local_repo_dir1='C:\\Users\\delor\\github\\french_language'
+
+
+if user=='delora_Win10':
+    local_data_dir=local_data_dir1
+    output_dir1=local_data_dir1
+    user_OS='Windows'
