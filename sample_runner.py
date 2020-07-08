689b74658e0dcca55e359149af14cdd39ff2f379
DeloraGaskins <delora.gaskins@gmail.com>
2020-07-08 22:58:26 +0200

    testing out recreating sentence from database

--- sample_runner.py
+++ sample_runner.py
@@ -4,11 +4,43 @@ import random
 #nltk.download()
 
 import methods.sentence_analysis as SA
+import methods.web_tools as web_tools
+
+import strategies.inputs_file as InF
+
+#######################################################################
+URL='https://savoirs.rfi.fr/fr/apprendre-enseigner/langue-fran%C3%A7aise/journal-en-francais-facile-03072020-20h00-gmt'
+#database builders
+filename=web_tools.journal_en_francais_facile_puller(URL)
+######################################################################
+
+list_of_sentences=InF.read_resource(filename)
+sentence=list_of_sentences[0]
+########################################################################
+ #sentence_based functions###
+
+#SA.recreate_sentence(sentence)
 
 
-sentence='Les États-Unis compte un nouveau candidat à la présidence.'
-sentence='Cette journée de dimanche est marquée par un double scrutin'
-#sentence='Avant cette annonce, la journée de dimanche a été l\'occasion pour le président  Emmanuel Macron et son nouveau Premier ministre Jean Castex de poursuivre les consultation, dans le but de constitué la nouvelle équipe gouvernementale'
-#sentence='L\'artiste, milliardaire a fait cette annonce sur Twitter'
 
-SA.recreate_sentence(sentence)
+#web_tools.lookup_words()
+#######################################################################
+multiplier=int(InF.user_inputter('type multipler'))
+baselist=[1,2,3,4,5,6,7,8,9,10]
+
+print('**********************')
+print('**********************')
+print('**********************')
+list_of_sentences=InF.read_resource(filename)
+which_sentences=[element * multiplier for element in baselist]
+for counter1 in which_sentences:
+    sentence=list_of_sentences[counter1]
+    sentence=sentence.replace("\n", "")
+    print(sentence)
+print(counter1)
+
+print('**********************')
+print('**********************')
+print('**********************')
+#SA.recreate_sentence(sentence)
+web_tools.lookup_words()
