2858a332057a4a7ce008179c476295c560075ebd
deloragaskins <delora.gaskins@gmail.com>
2020-07-15 17:31:06 +0200

    replaced control structure with commands from os

--- strategies/outputs_file.py
+++ strategies/outputs_file.py
@@ -5,22 +5,11 @@ import sys
 def outpath_websource(websourcechoice):
     parser = SafeConfigParser()
     parser.read('./setup/user.ini')
-    candidate='OS'
-    print(parser.has_section(candidate))
-    user_OS=parser.get('OS', 'user_OS')
-    user_OS=user_OS[1:-1]
-    print (user_OS)
-
+  
     if websourcechoice=='journal_en_francais_facile' or 'AZ_lyrics':
-        if user_OS=='Windows':
-            spacer_char='\\'
-        elif user_OS=='Ubuntu':
-            spacer_char='/'
-        else:
-            print('invalid OS')
         outpath_loc=parser.get('file_locations', 'local_data_dir')
-        outpath_loc=outpath_loc[1:-1]
-        outpath = outpath_loc+spacer_char+ websourcechoice +spacer_char
+        outpath = os.path.join(output_loc.split(os.sep) + websourcechoice) + os.sep
+        
         if not os.path.exists(outpath):
             os.makedirs(outpath)
     else:
