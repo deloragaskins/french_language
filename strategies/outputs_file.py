5c846f2c0a7b2abfef3f8eb17010a2477770f17b
deloragaskins <delora.gaskins@gmail.com>
2021-01-30 20:56:20 -0800

    fixed outpath

--- strategies/outputs_file.py
+++ strategies/outputs_file.py
@@ -5,11 +5,12 @@ import sys
 def outpath_websource(websourcechoice):
     parser = SafeConfigParser()
     parser.read('./setup/user.ini')
-  
+
     if websourcechoice=='journal_en_francais_facile' or 'AZ_lyrics':
         outpath_loc=parser.get('file_locations', 'local_data_dir')
-        outpath = os.path.join(output_loc.split(os.sep) + websourcechoice) + os.sep
-        
+        outpath_loc=outpath_loc[1:-1]
+        outpath = os.path.join(outpath_loc+os.sep + websourcechoice) + os.sep
+        print(outpath)
         if not os.path.exists(outpath):
             os.makedirs(outpath)
     else:
